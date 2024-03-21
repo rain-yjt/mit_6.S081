eb08469d4c50dcf4842d15ad749e0ea5c8754ce6
suzy <bae_suzy1994@163.com>
2024-03-21 22:23:34 +0800

    pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,39 @@
+#include "kernel/types.h"
+#include "user.h"
+
+#define READ 0
+#define WRITE 1
+
+int main(int argc,char *argv[]){
+    //pfd : parent pipe,cfd : child pipe
+    int pfd[2],cfd[2],pid;
+    char buf[1];
+
+    if(pipe(pfd) < 0 || pipe(cfd) < 0){
+        fprintf(2,"pipe error.\n");
+        exit(1);
+    }
+
+    if((pid = fork()) < 0){
+        fprintf(2,"fork error.\n");
+        exit(1);
+    }else if(pid == 0){
+        close(pfd[WRITE]);
+        close(cfd[READ]);
+        read(pfd[READ],buf,1);
+        printf("%d : receive ping\n",getpid());
+        write(cfd[WRITE]," ",1);
+        close(pfd[READ]);
+        close(cfd[WRITE]);
+        exit(0);
+    }else{
+        close(pfd[READ]);
+        close(cfd[WRITE]);
+        write(pfd[WRITE]," ",1);
+        read(cfd[READ],buf,1);
+        printf("%d : receive pong\n",getpid());
+        close(pfd[WRITE]);
+        close(cfd[READ]);
+        exit(0);
+    }
+}
